fff5db6db42ad0ff500195685cfd89fb33302038
Tamao <reneozbay3@gmail.com>
2024-02-22 20:48:59 +0100

    Added Factorial

--- app.c
+++ app.c
@@ -13,7 +13,10 @@ int main(void)
     	printf(" * : Multiplication\n");
     	printf(" / : Division\n");
     	printf(" s/S : Square Root\n");
-    	printf(" p/P : Power\n\n");
+    	printf(" p/P : Power\n");
+	printf(" f/F : Factorial\n");
+
+	printf("\n");
 	
 	printf("Enter operation (+, -, *, /, s or S, p or P): ");
 	scanf(" %c", &oper);
@@ -26,24 +29,28 @@ int main(void)
 			res = n1 + n2;
 			printf("\nResult: %f\n", res);
 			break;
+
 		case '-':
 			printf("Enter two numbers(space between): ");
 			scanf("%f %f", &n1, &n2);
 			res = n1 - n2;
 			printf("\nResult: %f\n", res);
 			break;
+
 		case '*':
 			printf("Enter two numbers(space between): ");
 			scanf("%f %f", &n1, &n2);
 			res = n1 * n2;
 			printf("\nResult: %f\n", res);
 			break;
+
 		case '/':
 			printf("Enter two numbers(space between): ");
 			scanf("%f %f", &n1, &n2);
 			res = n1 / n2;
 			printf("\nResult: %f\n", res);
 			break;
+
 		case 's':
 		case 'S':
 			printf("Enter a number: ");
@@ -58,12 +65,32 @@ int main(void)
 				printf("Cannot find a square root of a negative number\n");
 			}
 			break;
+
 		case 'p':
 		case 'P':
 			printf("Enter base and exponent(space between): ");
 			scanf("%f %f", &n1, &n2);
 			res = pow(n1, n2);
 			printf("\nResult: %f\n", res);
+			break;
+
+		case 'f':
+		case 'F':
+			res = 1;
+			printf("Enter a number: ");
+			scanf("%f", &n1);
+			if (n1 >= 0)
+			{
+				for (int i = 1; i <= n1; ++i)
+				{
+					res = res *= i;
+				}
+				printf("\nResult: %f\n", res);
+			}
+			else
+			{
+				printf("Invalid number");
+			}
 			break;
 		
 		default:
